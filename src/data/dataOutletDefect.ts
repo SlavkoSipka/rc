f7cb175bb5cf9294c42d7a25e8d563ee75107571
Bogdan Gradjanin <99250686+SlavkoSipka@users.noreply.github.com>
2026-03-10 18:09:33 +0100

    Set stock to 0 for Yokomo Super Dogfighter part

--- src/data/dataOutletDefect.ts
+++ src/data/dataOutletDefect.ts
@@ -7,7 +7,7 @@ export const outletDefectProducts: Product[] = [
     title: "Yokomo Super Dogfighter Rere YZ-870C Aluminium Bulkheads Blue Anodized OUTLET",
     price: 80.00,
     color: "Blue",
-    stock: 1,
+    stock: 0,
     description: "One pair (front and back) of Yokomo Super Dogfighter Rere YZ-870C Bulkheads BLUE Anodized\n\nThis bulkheads were designed to minimize slope and the geometry of the arm hangers are designed in such way that the angle of the hangers maches the kick up angle of the hingepin, which in other hands means that there is no need for shims ect. Machined with much tighter tolerances made of high grade 6082 aluminium, CNC machined up to the highest standards and afterwards hard anodized for maximum scratch resistance.\n\n\nCAUTION... the bulkheads will only fit the rere super dogfighter, the bulkheads will not fit the old yokomo YZ10 ect...\n\nPLEASE NOTE!!!\nScrews are not includeds\n\nIMPORTANT!!!\nOnly parts from the first picture are included, other pictures are only for reference.\n\n\n\nThis is NOT an original Yokomo product but we make the parts as good as possible to match the original.\n\nWe ensure that all parts are well packaged, all parcels are send trackable and we will provide the tracking number once the parcel is shipped.\n\nIf you need a quote for shipping to your country, feel free to ask.\nFor any other question don't hesitate to contact us.",
     images: [
       "/images/pla1.jpg",
