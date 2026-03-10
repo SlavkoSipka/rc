336bbb8df423bec24e7b78f35a147e18c8568131
Bogdan Gradjanin <99250686+SlavkoSipka@users.noreply.github.com>
2026-03-10 13:48:14 +0100

    Add Tamiya Top Force DF01 Aluminium J3 part
    Added a new product with details including image, id, title, price, stock, description, and color.

--- src/data/products.ts
+++ src/data/products.ts
@@ -925,6 +925,20 @@ export const products: Product[] = [
     ]
   },
   {
+    image: "/images/20210415_125544.jpg",
+    id: "tamiya-top-force-df01-red-j3",
+    title: "Tamiya Top Force DF01 Aluminium red alloy J3 part",
+    price: 20.99,
+    stock: 1,
+    description: "1piece of aluminium replacement reproduction J3 part from the Tamiya Top Force J parts tree. This piece replaces the original J3 part on your Top force. Made of high grade 6082 aluminium, RED anodized.\n\nScrews are not included.\n\nPLEASE NOTE!!!\nLAST PICTURE IS ONLY FOR REFERENCE, NO OTHER PARTS ARE INCLUDED BESIDE THE ALLOY J3 PART\n\nThis is NOT an original Tamiya product but we make the parts as good as possible to match the original.\n\nWe ensure that all parts are well packaged, all parcels are send trackable and we will provide the tracking number once the parcel is shipped.\n\nIf you need a quote for shipping to your country, feel free to ask.\nFor any other question don't hesitate to contact us.",
+    color: "Red",
+    images: [
+      "/images/20210415_125544.jpg",
+      "/images/20210415_125600.jpg",
+      "/images/s-l1600 (86).webp"
+    ]
+  },
+  {
     image: "/images/s-l1600 (74).webp",
     id: "tamiya-gearbox-reinforcement-set-red",
     title: "Tamiya red TA01, TA02, DF01 Aluminium gearbox reinforcement set",
