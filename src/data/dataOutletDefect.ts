ddb01c80eb6db925bbcc48f9be5663fca237946f
Bogdan Gradjanin <99250686+SlavkoSipka@users.noreply.github.com>
2026-03-17 18:14:11 +0100

    Update dataOutletDefect.ts

--- src/data/dataOutletDefect.ts
+++ src/data/dataOutletDefect.ts
@@ -15,5 +15,20 @@ export const outletDefectProducts: Product[] = [
       "/images/pla3.jpg",
       "/images/pla4.jpg"
     ]
+     },
+  {
+    image: "/images/20260317_180421.jpg",
+    id: "Team-Losi-XX4-Blue-Bell-Cranks-defect",
+    title: "Team Losi XX4 Blue Aluminium Bell Cranks Steering Arms Defect",
+    price: 35.00,
+    stock: 1,
+    description: "Once installed, the scratches wont be visible because the bearing\nOther then these, the bellcranks are fully functional\n\n1 pair of aluminium replacement reproduction bell cranks (61 and 63 - A-1550) for Team Losi XX4 made of high grade 6082 alloy, BLUE anodized.\n\nThese belcranks are a direct replacement for the part number 61 and 63 from the user manual and the Steering Linkage/Bellcrank Set (4WD) A-1550.\n\n\nOther pictures are only for recerence, you will receive one set of BLUE anodized bellcranks from the first picture, screws are not included.\n\nThis is NOT an original Tamiya product but we make the parts as good as possible to match the original.\n\nWe ensure that all parts are well packaged, all parcels are send trackable and we will provide the tracking number once the parcel is shipped.\n\nIf you need a quote for shipping to your country, feel free to ask.\nFor any other question don't hastate to contact us.",
+    color: "Blue",
+    images: [
+      "/images/20260317_180421.jpg",
+      "/images/cudan.jpg",
+      "/images/cudan1.jpg",
+      "/images/cudan2.jpg"
+    ]
   }
 ];
