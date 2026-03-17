1c7886cf8ae7e442d56add1f62b47f4f38fc938d
Bogdan Gradjanin <99250686+SlavkoSipka@users.noreply.github.com>
2026-03-17 18:16:22 +0100

    Update dataOutletDefect.ts

--- src/data/dataOutletDefect.ts
+++ src/data/dataOutletDefect.ts
@@ -2,25 +2,10 @@ import { Product } from '../types';
 
 export const outletDefectProducts: Product[] = [
   {
-    image: "/images/pla1.jpg",
-    id: "yokomo-super-dogfighter-rere-yz-870c-aluminium-bulkheads-blue-anodized-outlet",
-    title: "Yokomo Super Dogfighter Rere YZ-870C Aluminium Bulkheads Blue Anodized OUTLET",
-    price: 80.00,
-    color: "Blue",
-    stock: 0,
-    description: "One pair (front and back) of Yokomo Super Dogfighter Rere YZ-870C Bulkheads BLUE Anodized\n\nThis bulkheads were designed to minimize slope and the geometry of the arm hangers are designed in such way that the angle of the hangers maches the kick up angle of the hingepin, which in other hands means that there is no need for shims ect. Machined with much tighter tolerances made of high grade 6082 aluminium, CNC machined up to the highest standards and afterwards hard anodized for maximum scratch resistance.\n\n\nCAUTION... the bulkheads will only fit the rere super dogfighter, the bulkheads will not fit the old yokomo YZ10 ect...\n\nPLEASE NOTE!!!\nScrews are not includeds\n\nIMPORTANT!!!\nOnly parts from the first picture are included, other pictures are only for reference.\n\n\n\nThis is NOT an original Yokomo product but we make the parts as good as possible to match the original.\n\nWe ensure that all parts are well packaged, all parcels are send trackable and we will provide the tracking number once the parcel is shipped.\n\nIf you need a quote for shipping to your country, feel free to ask.\nFor any other question don't hesitate to contact us.",
-    images: [
-      "/images/pla1.jpg",
-      "/images/pla2.jpg",
-      "/images/pla3.jpg",
-      "/images/pla4.jpg"
-    ]
-     },
-  {
     image: "/images/20260317_180421.jpg",
     id: "Team-Losi-XX4-Blue-Bell-Cranks-defect",
     title: "Team Losi XX4 Blue Aluminium Bell Cranks Steering Arms Defect",
-    price: 35.00,
+    price: 25.00,
     stock: 1,
     description: "Once installed, the scratches wont be visible because the bearing\nOther then these, the bellcranks are fully functional\n\n1 pair of aluminium replacement reproduction bell cranks (61 and 63 - A-1550) for Team Losi XX4 made of high grade 6082 alloy, BLUE anodized.\n\nThese belcranks are a direct replacement for the part number 61 and 63 from the user manual and the Steering Linkage/Bellcrank Set (4WD) A-1550.\n\n\nOther pictures are only for recerence, you will receive one set of BLUE anodized bellcranks from the first picture, screws are not included.\n\nThis is NOT an original Tamiya product but we make the parts as good as possible to match the original.\n\nWe ensure that all parts are well packaged, all parcels are send trackable and we will provide the tracking number once the parcel is shipped.\n\nIf you need a quote for shipping to your country, feel free to ask.\nFor any other question don't hastate to contact us.",
     color: "Blue",
