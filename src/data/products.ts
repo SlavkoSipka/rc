2c22c0d66e909aa4f7b609c5a7e7a2485f082515
Bogdan Gradjanin <99250686+SlavkoSipka@users.noreply.github.com>
2026-04-02 19:16:57 +0200

    Update products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -108,6 +108,27 @@ export const products: Product[] = [
       "/images/129.webp",
       "/images/130.webp"
     ]
+     },
+  {
+    image: "/images/p18.webp",
+    id: "yokomo-super-dogfighter-rere-yz870c-red-bulkheads",
+    title: "Yokomo Super Dogfighter Rere YZ-870C Aluminium Bulkheads Anodized Red",
+    price: 139.99,
+    color: "Red",
+    stock: 1,
+    description: "One pair (front and back) of Yokomo Super Dogfighter Rere YZ-870C Bulkheads RED Anodized\n\nThis bulkheads were designed to minimize slope and the geometry of the arm hangers are designed in such way that the angle of the hangers maches the kick up angle of the hingepin, which in other hands means that there is no need for shims ect. Machined with much tighter tolerances made of high grade 6082 aluminium, CNC machined up to the highest standards and afterwards anodized for maximum scratch resistance.\n\nCAUTION... the bulkheads will only fit the rere super dogfighter, the bulkheads will not fit the old yokomo YZ10 ect...\n\nPLEASE NOTE!!!\nScrews are not includeds\n\nIMPORTANT!!!\nOnly parts from the first picture are included, other pictures are only for reference.\n\nThis is NOT an original Yokomo product but we make the parts as good as possible to match the original.\n\nWe ensure that all parts are well packaged, all parcels are send trackable and we will provide the tracking number once the parcel is shipped.\n\nIf you need a quote for shipping to your country, feel free to ask.\nFor any other question don't hesitate to contact us.",
+    images: [
+      "/images/p18.webp",
+      "/images/p19.webp",
+      "/images/123.webp",
+      "/images/124.webp",
+      "/images/125.webp",
+      "/images/126.webp",
+      "/images/127.webp",
+      "/images/128.webp",
+      "/images/129.webp",
+      "/images/130.webp"
+    ]
   },
   {
     image: "/images/118.webp",
